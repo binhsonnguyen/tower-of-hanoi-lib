bf959f130bd06d272ae66b7510de09bfd901f2e0
son <son@codegym.vn>
2024-07-11 15:25:34 +0700

    game create discs for first tower

--- src/entity/impl/game.ts
+++ src/entity/impl/game.ts
@@ -1,11 +1,25 @@
 import GameInterface from '../gameInterface'
 import TowerInterface from '../towerInterface'
+import GameOptions from '../gameOptions'
+import { Disc } from './disc'
+import Tower from './tower'
 
 export class Game implements GameInterface {
   private readonly _towers: Array<TowerInterface>
+  private readonly _options: GameOptions
 
-  constructor (towersCount: number) {
-    this._towers = new Array<TowerInterface>(towersCount)
+  constructor (options: GameOptions) {
+    this._options = options
+
+    this._towers = new Array<TowerInterface>(this._options.totalTowers)
+    for (let i = 0; i < this._options.totalTowers; i++) {
+      this._towers[i] = new Tower()
+    }
+
+    const firstTower = this.towers[0]
+    for (let size = this._options.totalDiscs; size >= 1; size--) {
+      firstTower.put(new Disc(size))
+    }
   }
 
   get towers (): TowerInterface[] {
--- test/entity/game.test.ts
+++ test/entity/game.test.ts
@@ -1,8 +1,22 @@
 import { expect, test } from '@jest/globals'
 import GameInterface from '../../src/entity/gameInterface'
 import { Game } from '../../src/entity/impl/game'
+import GameOptions from '../../src/entity/gameOptions'
+import EmptyTowerError from '../../src/entity/emptyTowerError'
 
 test('test create a new game', () => {
-  const game: GameInterface = new Game(3)
+  const gameOptions = new GameOptions(3, 16)
+  const game: GameInterface = new Game(gameOptions)
   expect(game.towers.length).toBe(3)
+})
+
+test('test game create discs for first tower', () => {
+  const gameOptions = new GameOptions(3, 16)
+  const game: GameInterface = new Game(gameOptions)
+  const firstTower = game.towers[0]
+
+  for (let i = 1; i <= gameOptions.totalDiscs; i++) {
+    expect(firstTower.pop()?.width).toBe(i)
+  }
+  expect(() => firstTower.pop()).toThrow(EmptyTowerError)
 })
